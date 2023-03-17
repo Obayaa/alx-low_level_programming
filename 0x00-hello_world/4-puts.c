7d35f84a03b8b5bb14af5babb960b5ffa8d56520
Appiah Dora <doraappiah2004@gmail.com>
2023-03-16 19:11:20 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program that prints  line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
