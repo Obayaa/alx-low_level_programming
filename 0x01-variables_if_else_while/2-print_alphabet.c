5263d67d5626e4c6b2c56c3a9631cd5b3822d1e1
Appiah Dora <doraappiah2004@gmail.com>
2023-03-17 20:05:51 -0700

    IF not 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: alwys 0
+ */
+
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; 1++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
