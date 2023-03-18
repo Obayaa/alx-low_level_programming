5d07c7633e836984e4e66129360c05f4840acee5
Appiah Dora <doraappiah2004@gmail.com>
2023-03-18 16:45:34 -0700

    He is good

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,7 @@ int main(void)
         for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 
-	for (i = 'A' ; i <= 'Z' ; i++
+	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar(i);
         putchar('\n');
         return (0);
