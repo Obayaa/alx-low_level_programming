3250b3b4a11fef17ae9a5d9f94009722404a27ce
Appiah Dora <doraappiah2004@gmail.com>
2023-03-18 17:04:30 -0700

    He is good

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char 1;
+
+	for(i = 'a' ; i <= 'z' ; i++)
+		if (i != q && i != e)
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
