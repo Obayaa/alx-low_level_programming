3ce50e05be0b162cbd3528562c4a65050a63b7eb
Appiah Dora <doraappiah2004@gmail.com>
2023-03-18 18:43:18 -0700

    He is good

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++){
+		if (i == 9)
+			putchar(i + '0');
+		else
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+	}
+	return (0);
+}
