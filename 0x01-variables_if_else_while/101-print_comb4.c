ca85f214614489f2d8b9b43a08448528e770b91b
Appiah Dora <doraappiah2004@gmail.com>
2023-03-19 16:11:16 -0700

    He is good

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int m;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for  (j = 0 ; j < 10 ; j++)
+		{
+			for (m = 0 ; m < 10 ; m++)
+			{
+				if (i < j && j < m)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(m + '0');
+					if (i + j + m != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}	
+	}
+putchar('\n');
+return (0);
+}
