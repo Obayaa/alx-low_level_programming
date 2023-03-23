9ab9f02f3afe413073f0740c59d7ba203a3aeae7
Appiah Dora <doraappiah2004@gmail.com>
2023-03-22 17:41:28 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * int _islower - tells if an inputted alphabet is lowercased
+ *
+ * Return: 1 if c is lowercase, 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
