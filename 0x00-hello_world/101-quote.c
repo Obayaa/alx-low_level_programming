85791ee7e7f95e6be686fa180d2b5084ff501a8d
Appiah Dora <doraappiah2004@gmail.com>
2023-04-07 16:55:03 -0700

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
-/*
- * main - 
- * Reaturn 1 (Success)
- */
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, sizeof(text) - 1);
 	return (1);
 }
